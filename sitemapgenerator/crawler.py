da94ef2ab60def0a1af294fa5afd86ca879bcd17
Andrew Griffiths <mail@andrewgriffithsonline.com>
2016-06-29 17:41:03 +0100

    get internal links

--- sitemapgenerator/crawler.py
+++ sitemapgenerator/crawler.py
@@ -1,12 +1,28 @@
+import re
+import pprint
 import requests
 from bs4 import BeautifulSoup
 
 
 class Crawler:
 
-    def __init__(self):
-        print("inited")
+    def __init__(self, domain):
+        self.set_domain(domain)
         self.links = {}
+
+    def set_domain(self, domain):
+        if not domain:
+            raise Exception('domain must be defined')
+
+        if not domain.startswith('http://') and \
+                not domain.startswith('https://'):
+            self.domain = 'http://' + domain
+
+        self.domain = domain
+        print('')
+        print('self.domain')
+        pprint.pprint(self.domain)
+        print('')
 
     def extract_links(self, contents):
         soup = BeautifulSoup(contents, 'html.parser')
@@ -17,11 +33,26 @@ class Crawler:
 
     def merge_links(self, links):
         for k, v in links.items():
+            # strip domain on internal links
+            if k.startswith(self.domain):
+                k = re.sub('^' + re.escape(self.domain), '', k)
             if k not in self.links:
                 self.links[k] = v
 
-    def run(self, url):
-        text = self.request_url(url)
+    def get_domain_links(self):
+        return {k: v for k, v in self.links.items() if not k.startswith('http')}
+
+    def get_unvisited_links(self):
+        return [k for k, v in self.get_domain_links().items() if 'visited' not in v]
+
+    def run(self, url='', recurse=False):
+        text = self.request_url(self.domain + url)
         links = self.extract_links(text)
         self.merge_links(links)
+
+        if recurse is True:
+            links_unvisited = self.get_unvisited_links()
+            if links_unvisited:
+                return self.run(links_unvisited[0], recurse)
+
         return self.links
--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,26 +1,27 @@
 import pytest
-# import pprint
 from sitemapgenerator.crawler import Crawler
 
 
-@pytest.mark.parametrize(('path', 'expected'), [
-    ('/html', {}),
+@pytest.mark.parametrize(('path', 'links_all', 'links_unvisited'), [
+    ('/html', {}, []),
     ('/encoding/utf8', {
         'http://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-demo.txt': {
             'soup': {}
         }
-    }),
+    }, []),
     ('/links/1/1', {
         '/links/1/0': {
             'soup': {}
         }
-    })
+    }, ['/links/1/0'])
 ])
-def test_extract_links(path, expected, httpbin):
-    c = Crawler()
-    res = c.run(httpbin + path)
-    assert res.keys() == expected.keys()
+def test_run_no_recursion(path, links_all, links_unvisited, httpbin):
+    c = Crawler(httpbin.url)
+    links_all = c.run(path)
 
-    for k, v in res.items():
+    assert links_all.keys() == links_all.keys()
+    for k, v in links_all.items():
         assert 'soup' in v
         assert 'visited' not in v
+
+    assert c.get_unvisited_links() == links_unvisited
