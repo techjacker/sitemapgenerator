b9a99950ba4e23e7d3f976adf9dfb443917740ff
Andrew Griffiths <mail@andrewgriffithsonline.com>
2016-06-30 10:48:50 +0100

    DRYer tests

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -2,37 +2,9 @@ import pytest
 from sitemapgenerator.crawler import Crawler
 
 
-@pytest.mark.parametrize(('path', 'links_all', 'links_unvisited'), [
-    ('/html', {}, []),
-    ('/encoding/utf8', {
-        'http://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-demo.txt': {
-            'soup': {}
-        }
-    }, []),
-    ('/links/1/1', {
-        '/links/1/0': {
-            'soup': {}
-        }
-    }, ['/links/1/0']),
-    ('/links/2/2', {
-        '/links/2/1': {
-            'soup': {}
-        },
-        '/links/2/0': {
-            'soup': {}
-        }
-    }, ['/links/2/1', '/links/2/0'])
-])
-def test_run(path, links_all, links_unvisited, httpbin):
-    c = Crawler(httpbin.url)
-    c.run(url=path, recurse=False)
-
-    assert c.links.keys() == links_all.keys()
-    for k, v in c.links.items():
-        assert 'soup' in v
-        assert 'visited' not in v
-
-    assert c.get_unvisited_links().sort() == links_unvisited.sort()
+@pytest.fixture(params=[(True, 0), (False, 10000)])
+def recurse(request):
+    return request.param
 
 
 @pytest.mark.parametrize(('path', 'links_all', 'links_unvisited'), [
@@ -56,14 +28,19 @@ def test_run(path, links_all, links_unvisited, httpbin):
         }
     }, ['/links/2/1', '/links/2/0'])
 ])
-def test_run_recursion(path, links_all, links_unvisited, httpbin):
+def test_run(path, links_all, links_unvisited, httpbin, recurse):
     c = Crawler(httpbin.url)
-    c.run(url=path, recurse=True, throttle=0)
+    c.run(url=path, recurse=recurse[0], throttle=recurse[1])
 
     assert c.links.keys() == links_all.keys()
     for k, v in c.links.items():
         assert 'soup' in v
-        if c.get_unvisited_links():
-            assert 'visited' in v
+        if recurse[0] is False:
+            assert 'visited' not in v
+        else:
+            if k.startswith('http'):
+                assert 'visited' not in v
+            else:
+                assert 'visited' in v
 
     assert c.get_unvisited_links().sort() == links_unvisited.sort()
